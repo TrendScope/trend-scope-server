48586e99d8d04adda3ea0ac5b4d1616aba585f9e
chaeyeon <codusl100@naver.com>
2024-03-12 17:11:44 +0900

    ✨ #10 Feat: 아이디 중복 체크 API

--- trend_scope/accounts/views.py
+++ trend_scope/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from accounts.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import APIException
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['POST'])
+def check_duplicate_id(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        exist_user = User.objects.filter(username=username)
+
+        if exist_user:
+            raise APIException(detail="이미 존재하는 아이디입니다.")
+        else:
+            return Response({'detail': "회원가입이 가능한 아이디입니다."}, status=status.HTTP_200_OK)
