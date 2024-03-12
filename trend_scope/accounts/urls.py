bf635da35892bd68043af7e5462073fabce3b649
chaeyeon <codusl100@naver.com>
2024-03-12 22:31:07 +0900

    :art: #12 Style: accounts 앱 url 수정

--- trend_scope/accounts/urls.py
+++ trend_scope/accounts/urls.py
@@ -4,8 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from accounts.views import check_duplicate_id
 
 urlpatterns = [
-    path('registration/', include('dj_rest_auth.registration.urls')),
+    path('registration', include('dj_rest_auth.registration.urls')),
     path('registration/id-check', check_duplicate_id, name='check_duplicate_id'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
